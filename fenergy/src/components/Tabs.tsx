6deaad0bd5f969fdf2144112e6e391aa07b91fc5
mdefalco <marcellodefalco0221@gmail.com>
2025-11-23 11:12:48 +0100

    Tabs | add new component reusable

---
+++ fenergy/src/components/Tabs.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+
+//Devo stabilire il numero di tab che possono apparire
+//decido di fare un array di oggetti , dove all'interno del componente verra utilizzato da un for
+
+interface TabsProps {
+  label?: string;
+  keys: string[];
+}
+
+export default function Tabs() {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            {/* {keys.map(())} */}
+            {/* <Tab key={}></Tab> */}
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
