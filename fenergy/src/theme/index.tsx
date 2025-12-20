a16c071aad379a8becde2ced11e8344eb38de3ef
mdefalco <marcellodefalco0221@gmail.com>
2025-12-20 16:39:22 +0100

    Default theme for the moment

--- fenergy/src/theme/index.tsx
+++ fenergy/src/theme/index.tsx
@@ -1,6 +1,8 @@
 import { createTheme } from "@mui/material/styles";
 import type {} from "@mui/x-data-grid/themeAugmentation";
 
+//TODO - Inserimento della palette di colori sia per la modalità light sia per la dark.
+//Implementare logica per lo switch del tema dentro l'applicazione
 export const theme = createTheme({
   cssVariables: { colorSchemeSelector: "class" },
   colorSchemes: { light: true, dark: true },
@@ -12,12 +14,32 @@ export const theme = createTheme({
       styleOverrides: {
         root: ({ theme }) => ({
           fontFamily: "Poppins, sans-serif",
-          backgroundColor: theme.palette.mode !== "dark" ? "#fff" : "#000",
-          color: theme.palette.mode !== "dark" ? "#fff" : "#0000",
+          backgroundColor: "#fff",
+          color: "black",
           "& .MuiToolbar-root": {
             backgroundColor:
               theme.palette.mode !== "dark" ? "#fff" : "#000 !important",
             color: theme.palette.mode !== "dark" ? "#fff" : "#000",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: "#fff",
+            color: "black",
+          },
+          "& .MuiTablePagination-displayedRows": {
+            backgroundColor: "#fff",
+            color: "black",
+          },
+        }),
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          "& .MuiToolbar-root": {
+            display: "flex",
+            justifyContent: "space-between",
+            backgroundColor: "white",
+            color: "black",
           },
         }),
       },
